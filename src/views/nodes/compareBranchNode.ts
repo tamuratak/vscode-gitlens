73246fa011ac43d6c6fca39c7dd341d4e2cda6ae
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-06-18 14:23:21 +0900

    Add my own tweaks

--- src/views/nodes/compareBranchNode.ts
+++ src/views/nodes/compareBranchNode.ts
@@ -352,7 +352,7 @@ export class CompareBranchNode extends ViewNode<BranchesView | CommitsView | Rep
 		} else if (this.compareWithWorkingTree) {
 			comparison = this._compareWith.ref;
 		} else {
-			comparison = `${this._compareWith.ref}..${this.branch.ref}`;
+			comparison = `${this._compareWith.ref}...${this.branch.ref}`;
 		}
 
 		const [filesResult, statsResult] = await Promise.allSettled([
